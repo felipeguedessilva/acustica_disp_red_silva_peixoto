c5d750c099dd65d61ce839a92d18436fb2cbbfe1
Felipe Silva <felipe_guedes@cfd01.ime.usp.br>
2024-11-15 23:18:04 -0300

    update2

---
+++ test_stencils_server/figures_exp/play_figures_norms.sh
@@ -0,0 +1,8 @@
+export DEVITO_ARCH=gcc
+export DEVITO_PLATFORM=intel64
+export DEVITO_LOGGING=DEBUG
+export DEVITO_LANGUAGE=openmp
+export DEVITO_AUTOTUNING=aggressive
+export OMP_NUM_THREADS=40
+
+nohup python test_figures_norms.py &
